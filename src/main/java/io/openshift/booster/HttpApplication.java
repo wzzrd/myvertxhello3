fb440aa7bce6f234ff682f885ac178f239454309
Maxim Burgerhout <maxim@wzzrd.com>
2017-12-04 20:15:51 +0000

    Yeah. Dawg.

--- src/main/java/io/openshift/booster/HttpApplication.java
+++ src/main/java/io/openshift/booster/HttpApplication.java
@@ -11,7 +11,7 @@ import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
 public class HttpApplication extends AbstractVerticle {
 
-  protected static final String template = "Meh from Che, %s! And all that.";
+  protected static final String template = "Meh from Che, %s! And all that. Dawg.";
 
   @Override
   public void start(Future<Void> future) {
